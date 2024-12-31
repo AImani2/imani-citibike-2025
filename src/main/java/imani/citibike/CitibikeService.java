fe53bd4c7086215a07779506ba454b2b2df33b96
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-12-30 21:42:55 -0500

    service work

---
+++ src/main/java/imani/citibike/CitibikeService.java
@@ -0,0 +1,13 @@
+package imani.citibike;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface CitibikeService {
+    @GET("station_information.json")
+    Single<Stations> getStationInfoResponse();
+
+    @GET("station_status.json")
+    Single<Stations> getStationStatusResponse();
+}
+
