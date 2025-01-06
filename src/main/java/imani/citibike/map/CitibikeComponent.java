a298442ba1cb1936a011cf46d50995a43a63f299
AImani2 <121688634+AImani2@users.noreply.github.com>
2025-01-06 09:43:35 -0500

    working on frame

--- src/main/java/imani/citibike/map/CitibikeComponent.java
+++ src/main/java/imani/citibike/map/CitibikeComponent.java
@@ -12,6 +12,8 @@ import org.jxmapviewer.viewer.TileFactoryInfo;
 
 import javax.swing.*;
 import javax.swing.event.MouseInputListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class CitibikeComponent extends JComponent {
     private final JXMapViewer mapViewer;
@@ -35,6 +37,36 @@ public class CitibikeComponent extends JComponent {
         mapViewer.addMouseListener(new CenterMapListener(mapViewer));
         mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mapViewer));
         mapViewer.addKeyListener(new PanKeyListener(mapViewer));
+
+        /*mapViewer.addPropertyChangeListener("zoom", new PropertyChangeListener()
+        {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt)
+            {
+                updateWindowTitle(frame, mapViewer);
+            }
+        });
+
+        mapViewer.addPropertyChangeListener("center", new PropertyChangeListener()
+        {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt)
+            {
+                updateWindowTitle(frame, mapViewer);
+            }
+        });
+
+        updateWindowTitle(frame, mapViewer);
+    }*/
+
+//    protected static void updateWindowTitle(JFrame frame, JXMapViewer mapViewer)
+//    {
+//        double lat = mapViewer.getCenterPosition().getLatitude();
+//        double lon = mapViewer.getCenterPosition().getLongitude();
+//        int zoom = mapViewer.getZoom();
+//
+//        frame.setTitle(String.format("JXMapviewer2 Example 3 (%.2f / %.2f) - Zoom: %d", lat, lon, zoom));
+//    }
 
     }
 }
--- src/main/java/imani/citibike/map/CitibikeFrame.java
+++ src/main/java/imani/citibike/map/CitibikeFrame.java
@@ -1,22 +1,56 @@
 package imani.citibike.map;
 
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.viewer.Waypoint;
+import org.jxmapviewer.viewer.WaypointPainter;
+
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class CitibikeFrame extends JFrame {
 
+    private final CitibikeController controller;
     public CitibikeFrame() {
+        RoutePainter routePainter = new RoutePainter();
+        WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<>();
+        CitibikeComponent mapViewer = new CitibikeComponent();
+        controller = new CitibikeController();
+
         setTitle("Citibike Map");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        getContentPane().add(mapViewer); //add(mapViewer, BorderLayout.CENTER);
 
-        //getContentPane().add(mapViewer);
+        JPanel buttonPanel = new JPanel();
+
+        JButton mapButton = new JButton("Map");
+        buttonPanel.add(mapButton);
+
+        JButton clearButton = new JButton("Clear");
+        buttonPanel.add(clearButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        JPanel coordPanel = new JPanel();
+        coordPanel.setLayout(new GridLayout(0, 1));
+
+        JLabel toLabel = new JLabel("To: ");
+        toLabel.setBorder(new LineBorder(Color.BLACK));
+        toLabel.setBackground(Color.WHITE);
+        toLabel.setOpaque(true);
+        coordPanel.add(toLabel);
+
+        JLabel fromLabel = new JLabel("From: ");
+        fromLabel.setBorder(new LineBorder(Color.BLACK));
+        fromLabel.setBackground(Color.WHITE);
+        fromLabel.setOpaque(true);
+        coordPanel.add(fromLabel);
+        add(coordPanel, BorderLayout.NORTH);
+
+        // now I have to add my mouse listener to the map
+        // and i have to add my action listeners to the buttons
+        // i call my controller from this class
     }
-
-    /*
-        what i need to do in this class:
-        i want the jmap on the center
-        then on the bottom buttons
-     */
-
-
 }
