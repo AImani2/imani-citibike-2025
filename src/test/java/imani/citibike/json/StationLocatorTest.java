242131816fb3895cd8764fd9ad50a6311f29a45e
AImani2 <121688634+AImani2@users.noreply.github.com>
2025-01-08 21:20:13 -0500

    readme and tests

--- src/test/java/imani/citibike/json/StationLocatorTest.java
+++ src/test/java/imani/citibike/json/StationLocatorTest.java
@@ -1,4 +1,44 @@
 package imani.citibike.json;
 
+import imani.citibike.service.CitibikeService;
+import imani.citibike.service.CitibikeServiceFactory;
+import imani.citibike.service.StationUpdaterService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StationLocatorTest {
+
+    @Test
+    public void findClosestPickUpStation() {
+        //given
+        CitibikeService citibikeService = new CitibikeServiceFactory().getService();
+        StationUpdaterService sus = new StationUpdaterService(citibikeService);
+        StationLocator stationLocator = new StationLocator();
+
+        //when
+        Station startStation = stationLocator.findClosestStation(-73.9359, 40.8211, true, sus);
+
+        //then
+        assertNotNull(startStation);
+        assertTrue(startStation.num_bikes_available > 0);
+        assertEquals("Lenox Ave & W 146 St", startStation.name);
+    }
+
+    @Test
+    public void findClosestDockingStation() {
+        //given
+        CitibikeService citibikeService = new CitibikeServiceFactory().getService();
+        StationUpdaterService sus = new StationUpdaterService(citibikeService);
+        StationLocator stationLocator = new StationLocator();
+
+        //when
+        Station endStation = stationLocator.findClosestStation(-73.9585, 40.7190, true, sus);
+
+        //then
+        assertNotNull(endStation);
+        assertTrue(endStation.num_docks_available > 0);
+        assertEquals("Berry St & N 8 St", endStation.name);
+
+    }
 }
